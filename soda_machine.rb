833331b4bfe76bee0b2e49e660233253aa03858c
Robin Soubry <robin.soubry@gmail.com>
2016-05-17 19:24:02 -0700

    Add inventory method and find soda method

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,9 +7,12 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    found_soda = @sodas.select { |soda| soda.brand == soda_brand }
+    found_soda[0]
   end
 
   def sell(soda_brand)
